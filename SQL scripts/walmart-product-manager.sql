542e390d0f5612f08b73d9a4187ca74435b1e1c2
DRRoche <123427914+DRRoche@users.noreply.github.com>
2024-10-03 10:29:33 -0400

    Update walmart-product-manager.sql

--- SQL scripts/walmart-product-manager.sql
+++ SQL scripts/walmart-product-manager.sql
@@ -3,14 +3,14 @@ create database Walmart_Product_Manager;
 use Walmart_Product_Manager;
 
 create table department(
-dept_ID char(5), -- Change to enum
-dept_name char(20) NOT NULL,
+dept_ID char(5),
+dept_name varchar(20) NOT NULL,
 primary key (dept_ID));
 
 create table product(
 SKU int,
-dept_ID char(5), -- Change to enum
-prod_name char(20) NOT NULL,
+dept_ID char(5),
+prod_name varchar(20) NOT NULL,
 size int NOT NULL,
 unit ENUM('oz', 'lb', 'pcs') NOT NULL,
 price numeric(8,2) NOT NULL,
@@ -20,30 +20,30 @@ foreign key (dept_ID) references department(dept_ID));
 create table store(
 store_ID int,
 street_num int NOT NULL,
-street_name char(20) NOT NULL,
-city char(20) NOT NULL,
-state char(20) NOT NULL,
+street_name varchar(20) NOT NULL,
+city varchar(20) NOT NULL,
+state varchar(20) NOT NULL,
 zip_code int NOT NULL,
 primary key (store_ID));
 
 create table brand(
 brand_ID char(5),
-brand_name char(20) NOT NULL,
+brand_name varchar(20) NOT NULL,
 chain_exclusive bool,
 primary key (brand_ID));
 
 create table employee(
 E_ID int,
 M_ID int,
-e_name char(20) NOT NULL,
-position char(20) NOT NULL,
+e_name varchar(20) NOT NULL,
+position char(20) NOT NULL, -- Change to enum
 payroll_type ENUM('Hourly', 'Salary') NOT NULL,
 pay_rate numeric(9,2) NOT NULL,
 primary key (E_ID));
 
 create table store_departments(
 store_ID int,
-dept_id char(5), -- Change to enum
+dept_id char(5), 
 primary key (store_ID, dept_ID),
 foreign key (store_ID) references store(store_ID),
 foreign key (dept_ID) references department(dept_ID));
@@ -63,7 +63,7 @@ foreign key (brand_ID) references brand(brand_ID),
 foreign key (SKU) references product(SKU));
 
 create table works_in(
-dept_ID char(5), -- Change to enum
+dept_ID char(5), 
 E_ID int,
 primary key (dept_ID, E_ID),
 foreign key (dept_ID) references department(dept_ID),
