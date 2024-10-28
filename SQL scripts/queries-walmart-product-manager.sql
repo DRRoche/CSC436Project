3da7ed4a799ca7a48257d4f728e465e27bcc6cef
DRRoche <123427914+DRRoche@users.noreply.github.com>
2024-10-28 13:01:10 -0400

    Added queries for demo
    - product list by store view
    - all products stocked at all stores
    - all products at a given store
    - products containing a string portion

--- SQL scripts/queries-walmart-product-manager.sql
+++ SQL scripts/queries-walmart-product-manager.sql
@@ -1,19 +1,42 @@
-select * 
-from product;
+-- List all products
+select * from product;
+select * from employee;
 
 -- unit price view
 create view unit_price as
 select SKU, dept_id, prod_name, size, price, round(price / size, 2) as unit_price, unit
 from product;
 
+-- creates a view with products listed uniquely to stores
+create view prod_list_by_store as
+select prod_name, SKU, unit_price.dept_ID, price, unit_price, unit, store.store_id
+from unit_price
+natural join  stocks natural join store
+order by prod_name;
+
+select * from unit_price;
+select * from prod_list_by_store;
+
+
+-- get all products stocked at every store
+with store_cnt(total) as (select count(*) from store) 
 select *
-from unit_price;
+from prod_list_by_store
+where exists(
+	select SKU, cnt
+    from(
+		select SKU, count(store_id) as cnt 
+        from prod_list_by_store 
+        group by SKU) as a 
+	where a.cnt = (select total from store_cnt));
+    
+-- get all products at a given store
+select * 
+from prod_list_by_store
+where store_id = 4;
 
-select * from employee;
+-- find products containing <string>
+select *
+from unit_price
+where prod_name like "%oil%";
 
-select e_id, e_name
-from employee
-where e_id not in(
-	select m_id
-	from employee
-	where m_id is not null);
