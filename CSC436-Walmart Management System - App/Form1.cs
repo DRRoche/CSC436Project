2c215b523d4ccb57e0f05fee5859ead8185ce136
DRRoche <123427914+DRRoche@users.noreply.github.com>
2024-11-02 09:40:38 -0400

    .gitignore and windows forms app template
    - sets up essential .gitingore for visual studio community

---
+++ CSC436-Walmart Management System - App/Form1.cs
@@ -0,0 +1,10 @@
+namespace CSC436_Walmart_Management_System___App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
