151ea1433f9bde3bc3d182a97086b3ed4fadfa4c
DRRoche <123427914+DRRoche@users.noreply.github.com>
2024-12-02 16:59:59 -0500

    Update Main_Menu.Designer.cs

--- CSC436-Walmart Management System - App/Main_Menu.Designer.cs
+++ CSC436-Walmart Management System - App/Main_Menu.Designer.cs
@@ -26,8 +26,7 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             prod_search_btn = new Button();
             emp_search_btn = new Button();
             emp_man_btn = new Button();
@@ -45,7 +44,6 @@
             // 
             // emp_search_btn
             // 
-            emp_search_btn.Enabled = false;
             emp_search_btn.Location = new Point(82, 138);
             emp_search_btn.Name = "emp_search_btn";
             emp_search_btn.Size = new Size(148, 44);
@@ -56,7 +54,6 @@
             // 
             // emp_man_btn
             // 
-            emp_man_btn.Enabled = false;
             emp_man_btn.Location = new Point(82, 229);
             emp_man_btn.Name = "emp_man_btn";
             emp_man_btn.Size = new Size(148, 44);
